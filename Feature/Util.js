132c35116ad6d6a5d052d72baeee3e1a90d4342c
V-Dat <vandat.k21@gmail.com>
2022-11-02 21:07:21 +0700

    Re-located folder

--- Feature/Util.js
+++ Feature/Util.js
@@ -26,48 +26,48 @@ export function unActiveButtonEdit(rowNode) {
   if (!rowNode) return;
   const editNote = rowNode.querySelector(".edit");
   editNote.classList.remove("active");
-  editNote.src = "./Assets/edit-unactive-icon.svg";
+  editNote.src = "./Assets/Icons/edit-unactive-icon.svg";
 }
 
 export function activeButtonEdit(rowNode) {
   if (!rowNode) return;
   const editNote = rowNode.querySelector(".edit");
   editNote.classList.add("active");
-  editNote.src = "./Assets/edit-active-icon.svg";
+  editNote.src = "./Assets/Icons/edit-active-icon.svg";
 }
 
 export function activeButtonSave(rowNode) {
   if (!rowNode) return;
   const saveNode = rowNode.querySelector(".save");
   saveNode.classList.add("active");
-  saveNode.src = "./Assets/create-active-icon.svg";
+  saveNode.src = "./Assets/Icons/create-active-icon.svg";
 }
 export function unActiveButtonSave(rowNode) {
   if (!rowNode) return;
   const saveNode = rowNode.querySelector(".save.active");
   saveNode.classList.remove("active");
-  saveNode.src = "./Assets/create-unactive-icon.svg";
+  saveNode.src = "./Assets/Icons/create-unactive-icon.svg";
 }
 
 export function activeButtonCreate(rowNode) {
   if (!rowNode) return;
   const createNode = rowNode.querySelector(".save-new-record");
   createNode.classList.add("active");
-  createNode.src = "./Assets/create-active-icon.svg";
+  createNode.src = "./Assets/Icons/create-active-icon.svg";
 }
 export function unActiveButtonCreate(rowNode) {
   if (!rowNode) return;
   const createNode = rowNode.querySelector(".save-new-record.active");
   if (!createNode) return;
   createNode.classList.remove("active");
-  createNode.src = "./Assets/create-unactive-icon.svg";
+  createNode.src = "./Assets/Icons/create-unactive-icon.svg";
 }
 
 export function activeButtonEraserNewNode(rowNode) {
   if (!rowNode) return;
   const eraserNewNode = rowNode.querySelector(".clear-new-node");
   eraserNewNode.classList.add("active");
-  eraserNewNode.src = "./Assets/eraser-active-icon.svg";
+  eraserNewNode.src = "./Assets/Icons/eraser-active-icon.svg";
 }
 
 export function unActiveButtonEraserNewNode(rowNode) {
@@ -75,39 +75,39 @@ export function unActiveButtonEraserNewNode(rowNode) {
   const eraserNewNode = rowNode.querySelector(".clear-new-node.active");
   if (!eraserNewNode) return;
   eraserNewNode.classList.remove("active");
-  eraserNewNode.src = "./Assets/eraser-unactive-icon.svg";
+  eraserNewNode.src = "./Assets/Icons/eraser-unactive-icon.svg";
 }
 export function activeButtonEraserInput(node) {
   if (!node) return;
   const eraserNode = node.querySelector("img.eraser");
-  eraserNode.src = "./Assets/eraser-active-icon.svg";
+  eraserNode.src = "./Assets/Icons/eraser-active-icon.svg";
 }
 
 export function activeButtonUndo(node) {
   if (!node) return;
   const undoNode = node.querySelector("img.undo");
   undoNode.classList.add("active");
-  undoNode.src = "./Assets/undo-active-icon.svg";
+  undoNode.src = "./Assets/Icons/undo-active-icon.svg";
 }
 
 export function activeButtonDelete(node) {
   if (!node) return;
   const eraserNode = node.querySelector("img.eraser");
-  eraserNode.src = "./Assets/delete-icon.svg";
+  eraserNode.src = "./Assets/Icons/delete-icon.svg";
 }
 
 export function unActiveButtonUndo() {
   const undoNode = document.querySelector("img.undo.active");
   if (!undoNode) return;
   undoNode.classList.remove("active");
-  undoNode.src = "./Assets/undo-unactive-icon.svg";
+  undoNode.src = "./Assets/Icons/undo-unactive-icon.svg";
 }
 
 export function activeAllButtonEdit() {
   const buttonActiveNode = $(".button-actions img:not(.active).edit");
   if (!buttonActiveNode) return;
   buttonActiveNode.classList.add("active");
-  buttonActiveNode.src = "./Assets/edit-active-icon.svg";
+  buttonActiveNode.src = "./Assets/Icons/edit-active-icon.svg";
 }
 
 export function decodeInnerHTML(str) {
@@ -121,11 +121,11 @@ export function highlightNode(node) {
 }
 
 export function activeButtonSaveEditContent(button) {
-  button.src = "./Assets/save-main-content-icon.svg";
+  button.src = "./Assets/Icons/save-main-content-icon.svg";
   button.title = "save content";
 }
 
 export function activeButtonEditContent(button) {
-  button.src = "./Assets/pen-icon.svg";
+  button.src = "./Assets/Icons/pen-icon.svg";
   button.title = "edit content";
 }
