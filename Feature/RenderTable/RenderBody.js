5b8901a8c4129108175a17862b06f14b2ccc943c
V-Dat <vandat.k21@gmail.com>
2022-11-13 01:55:12 +0700

    fix: fixbug render note in homepage

--- Feature/RenderTable/RenderBody.js
+++ Feature/RenderTable/RenderBody.js
@@ -10,19 +10,23 @@ export function renderTableBody(app) {
 function processRow(data) {
   if (!data) return;
   let tableRow = "";
-
   data.forEach((row, rowIndex) => {
     tableRow += `<tr data-row-index=${rowIndex}>`;
-    row.forEach((cell, cellIndex) => {
-      tableRow += `<td type="cell" data-row-type="row-data" data-row-index=${
-        rowIndex + 2
-      } data-column-index=${cellIndex} data-column-name=${cell.name} class="${
-        cell.index === 0 || cell.index === row.length - 1
-          ? "cell"
-          : "column-data cell"
-      }" style="background-color:${cell.bg || "color"}" >${
-        cell.data ? cell.data : getButtonEdit(rowIndex, cellIndex)
-      }</td>`;
+    let cellIndex = 0;
+    row.forEach((cell) => {
+      if (cell.show.includes("home") || cell.show.includes("default-home")) {
+        tableRow += `<td type="cell" data-row-type="row-data" data-row-index=${
+          rowIndex + 2
+        } data-column-index=${cellIndex} data-column-name=${cell.name} class="${
+          cell.index === 0 || cell.index === row.length - 1
+            ? "cell"
+            : "column-data cell"
+        }" style="background-color:${cell.bg || "color"}" >${
+          cell.data ? cell.data : getButtonEdit(rowIndex, cellIndex)
+        }</td>`;
+
+        cellIndex++;
+      }
     });
     tableRow += "</tr>";
   });
