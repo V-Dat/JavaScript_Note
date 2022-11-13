f32f7efc2f9575df16f2c095090cff9604e3ca3c
V-Dat <vandat.k21@gmail.com>
2022-11-14 04:08:52 +0700

    docs: add Prettier Json

---
+++ Feature/MenuBar/MenuBarData.js
@@ -0,0 +1,54 @@
+export const DOCS = [
+    {
+        name: 'Default',
+        key: 'Default',
+    },
+    {
+        name: 'Boolean',
+        key: 'Boolean',
+    },
+    {
+        name: 'Iterator',
+        key: 'Iterator',
+    },
+    {
+        name: 'Function',
+        key: 'Function',
+    },
+    {
+        name: 'HTTP',
+        key: 'HTTP',
+    },
+    {
+        name: 'String',
+        key: 'String',
+    },
+    {
+        name: 'Collections',
+        key: 'Collections',
+    },
+    {
+        name: 'Builtin Function',
+        key: 'Builtin_Function',
+    },
+    {
+        name: 'AJAX',
+        key: 'AJAX',
+    },
+    {
+        name: 'Code Formatter Prettier and ESlint',
+        key: 'Code_Formatter',
+    },
+    {
+        name: 'SPA - Routing vanilla (NoConfig)',
+        key: 'Default',
+    },
+    {
+        name: 'Git - (NoConfig)',
+        key: 'Default',
+    },
+    {
+        name: 'Upload',
+        key: 'Upload',
+    },
+];
